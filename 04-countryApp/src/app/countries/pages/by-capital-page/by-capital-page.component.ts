24c450ee667ff3091116337d18d0bfe92e4a2040
Maryny Castada <mzcastada@indra.es>
2023-07-18 17:04:21 +0200

    Countries Routing Module

---
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

1ac4f4b5468c3e39f9bd5d7947db0b06e62587f0
Maryny Castada <mzcastada@indra.es>
2023-07-19 13:04:13 +0200

    Debounce

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Input()
   public placeholder: string = '';
@@ -12,7 +16,26 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+        this.onDebounce.emit(value);
+      });
+  }
+
   emitValue(value: string): void {
     this.onValue.emit(value);
   }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
+  }
 }
+
+

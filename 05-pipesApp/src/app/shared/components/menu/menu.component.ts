38df85005640e74e7433f1ba4464ada5db89ea3a
Maryny Castada <mzcastada@indra.es>
2023-11-05 13:48:18 +0100

    Navegar usando el MenuBar

--- 05-pipesApp/src/app/shared/components/menu/menu.component.ts
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -17,9 +17,9 @@ export class MenuComponent implements OnInit {
             label: 'Pipes de Angular',
             icon: 'pi pi-desktop',
             items: [
-              { label: 'Textos y Fechas', icon: 'pi pi-align-left' },
-              { label: 'Números', icon: 'pi pi-dollar' },
-              { label: 'No comunes', icon: 'pi pi-globe' },
+              { label: 'Textos y Fechas', icon: 'pi pi-align-left', routerLink: ['/'] },
+              { label: 'Números', icon: 'pi pi-dollar', routerLink: 'numbers' },
+              { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'uncommon' },
             ]
           },
           {

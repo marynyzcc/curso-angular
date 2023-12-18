5a91e9e3e58c3e6f1787063f35ac4863eda1cb73
Maryny Castada <mzcastada@indra.es>
2023-12-18 12:37:48 +0100

    i18nPluralPipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -19,4 +19,17 @@ export class UncommonPageComponent {
     this.name = "John";
     this.gender = 'male';
   }
+
+  //i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
+
+  deleteClient() {
+    this.clients.shift();
+  }
 }

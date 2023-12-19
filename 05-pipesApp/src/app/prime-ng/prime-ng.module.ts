cd7209ac84a5b0baa55531413f573c4d0c92a1bb
Maryny Castada <mzcastada@indra.es>
2023-12-19 12:32:06 +0100

    Valores y argumentos personalizados al Pipe

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -5,7 +5,7 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
-
+import { ToolbarModule } from 'primeng/toolbar';
 @NgModule({
   exports: [
     //MenuModule,
@@ -13,7 +13,8 @@ import { PanelModule } from 'primeng/panel';
     ButtonModule,
     CardModule,
     FieldsetModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }

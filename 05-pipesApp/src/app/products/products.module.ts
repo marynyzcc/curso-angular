a015f55122f6ab9599caa9a1034dba39a346691f
Maryny Castada <mzcastada@indra.es>
2023-12-19 12:58:50 +0100

    Tarea- CanFly Pipe

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -8,6 +8,7 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ToggleCasePipe } from './pipes/toggle-case.pipe';
     OrderComponent,
 
     //Pipes
-    ToggleCasePipe
+    ToggleCasePipe,
+    CanFlyPipe
   ],
   imports: [
     CommonModule,

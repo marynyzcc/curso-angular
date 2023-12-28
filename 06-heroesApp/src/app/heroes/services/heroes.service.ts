4ff6a19361fae243a1ac8812970b5ee8bf533ed2
Maryny Castada <mzcastada@indra.es>
2023-12-28 15:17:59 +0100

    Obtener Héroe por URL

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -15,4 +15,10 @@ export class HeroesService {
     return this.httpClient.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
+  getHeroById(id: string): Observable<Hero|undefined> {
+    return this.httpClient.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
+  }
 }

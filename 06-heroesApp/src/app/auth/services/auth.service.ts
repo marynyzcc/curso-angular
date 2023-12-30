606b10dc24aa4dd0d3e74e0659faf59cc6c2dd12
Maryny Castada <mzcastada@indra.es>
2023-12-30 19:39:05 +0100

    AuthService - Servicio de Autenticación

---
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from 'src/environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser(): User | undefined {
+    if(!this.user) return undefined;
+    return structuredClone(this.user);  //structuredClone() realiza un deep clone del objeto que se pasa
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', user.id.toString()))
+      );
+  }
+
+}

3241232515ae119fbc5400dbb0db9a98bfd08ca3
Maryny Castada <mzcastada@indra.es>
2023-12-30 19:50:36 +0100

    Autenticar y mostrar usuario activo

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -21,8 +21,13 @@ export class AuthService {
     return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
-        tap(user => localStorage.setItem('token', user.id.toString()))
+        tap(user => localStorage.setItem('token', 'asdfrewg.afdfe.hjthre123'))
       );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 
 }

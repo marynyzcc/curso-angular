7604031a358939c80964224ada2d1266a1e8e32b
Maryny Castada <mzcastada@indra.es>
2023-12-30 20:43:12 +0100

    Implementar la funcionalidad del Guard

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -25,9 +25,9 @@ export class AuthService {
       );
   }
 
-  checkAuthentication(): Observable<boolean> | boolean {
+  checkAuthentication(): Observable<boolean> {
 
-    if(!localStorage.getItem('token')) return false;
+    if(!localStorage.getItem('token')) return of(false);
 
     const token = localStorage.getItem('token');
 

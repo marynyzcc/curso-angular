b280cd52b1f94903bd0758c6dcd3ad18c45665ea
Maryny Castada <mzcastada@indra.es>
2023-12-31 13:24:28 +0100

    Primeros pasos en formularios reactivos

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave() {
+    console.log(this.myForm.value);
+  }
 }

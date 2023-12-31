761bf026a782734073fc6cb62d49d572668ce372
Maryny Castada <mzcastada@indra.es>
2023-12-31 14:21:08 +0100

    Formularios dinámicos

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array([
+      ['Mario Bros', Validators.required],
+      ['Pokemon', Validators.required]
+    ]),
+
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get favouriteGames() {
+    return this.myForm.get('favouriteGames') as FormArray;
+  }
+
+  onSubmit() {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }

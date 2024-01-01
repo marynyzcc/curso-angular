6d37ea6cb3d05c3c89185851e085c4a7928f8cea
Maryny Castada <mzcastada@indra.es>
2024-01-01 17:15:12 +0100

    Verificar que dos campos sean iguales

--- 07-formApp/src/app/shared/service/validators.service.ts
+++ 07-formApp/src/app/shared/service/validators.service.ts
@@ -18,8 +18,24 @@ export class ValidatorsService {
     return null;
   }
 
-  isValidField(form: FormGroup, field: string): boolean | null {
+  public isValidField(form: FormGroup, field: string): boolean | null {
     return form.controls[field].errors && form.controls[field].touched;
+  }
+
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: FormGroup): ValidationErrors | null => {
+
+      const fieldValue1 = formGroup.get(field1)?.value || '';
+      const fieldValue2 = formGroup.get(field2)?.value || '';
+
+      if(fieldValue1 !== fieldValue2) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true };
+      }
+
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
   }
 
 

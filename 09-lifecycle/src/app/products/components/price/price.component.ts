24e7ac7c112df5bd696435f6be13909f2694125c
Maryny <marynycoll27@gmail.com>
2024-01-03 13:22:26 +0100

    ngOnChanges

--- 09-lifecycle/src/app/products/components/price/price.component.ts
+++ 09-lifecycle/src/app/products/components/price/price.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 
+  @Input()
+  public price: number = 0;
+
+  ngOnInit(): void {
+    console.log('Componente HIJO: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO: ngOnChanges');
+    console.log({changes});
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente HIJO: ngOnDestroy');
+  }
 }
--- 09-lifecycle/src/app/products/pages/product/product.component.ts
+++ 09-lifecycle/src/app/products/pages/product/product.component.ts
@@ -8,6 +8,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
 
   constructor() {
     console.log('Contructor');
@@ -46,5 +47,7 @@ export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterConten
     console.log('ngOnDestroy');
   }
 
-
+  increasePrice() {
+    this.currentPrice++;
+  }
 }
